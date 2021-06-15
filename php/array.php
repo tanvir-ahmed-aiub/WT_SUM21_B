334173a17ed7ded2acc04f99c6a962e9d383b078
taanveerahmeed <taanveerahmeed@gmail.com>
2021-06-15 13:58:38 +0600

    form submission

--- php/array.php
+++ php/array.php
@@ -23,9 +23,28 @@
 		echo $e.'<br>';
 	}
 	
-	//associate array
+	//associative array
 	$arr4= array();
 	$arr5 = [];
+	$arr6= array("tanvir" => 34,"sabbir"=>35);
+	
+	$arr4['tanvir'] = "35";
+	$arr4["sabbir"] = "36";
+	$arr4["karim"] = "37";
+	
+	echo "<br><br>";
+	
+	foreach($arr4 as $k){
+		echo $k;
+	}
+	echo "<br><br>";
+	foreach($arr4 as $k=>$v){
+		echo "Key: $k and value $v<br>";
+		//echo $arr4[$k];
+	}
+	
+	
+	
 	
 	
 	
