8066abef2bef1e05b7d47402d1eab30522a698dc
taanveerahmeed <taanveerahmeed@gmail.com>
2021-07-27 13:51:40 +0600

    js

--- CSS/index.php
+++ CSS/index.php
@@ -1,53 +1,11 @@
 <html>
 	<head>
+		<link rel="stylesheet" href="styles/mystyle.css">
 		<style>
-			p{
-				text-align:justify;
-				/*margin-top:5px;
-				margin-right:20px;
-				margin-left:20px;
-				margin-bottom:5px;*/
-				margin:10px;
-			}
-			
 			#p1{
-				background-color:blue;
-				padding:10px;
-			}
-			#p10{
-				background-color:#efefef;
-			}
-			#p5{
-				background-color:rgb(4,170,107);
-				font-family:consolas;
-			}
-			.red{
 				background-color:red;
-				font-family:verdana;
+				margin:20px;
 			}
-			.black{
-				background-color:#000000;
-				color:#ffffff;
-			}
-			.btn-link{
-				
-				background-color:rgb(73,127,175);
-				color:white;
-				padding-left:20px;
-				padding-right:20px;
-				padding-top:10px;
-				padding-bottom:10px;
-				border-radius:2px;
-				font-family:calibri;
-				text-decoration:none;
-			}
-			.btn-link:hover{
-				background-color:rgb(0,78,162);
-			}
-			
-			
-			
-			
 		</style>
 	</head>
 	<body>
@@ -58,7 +16,7 @@
 			<a href="" class="btn-link">Link3</a>
 		</div>
 		<br><br>
-		<p id="p1">Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document.</p>
+		<p id="p1" style="background-color:green">Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document.</p>
 		
 		<p class="black">To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the elements you want from the different galleries.</p>
 		<p class="black">Themes and styles also help keep your document coordinated. When you click Design and choose a new Theme, the pictures, charts, and SmartArt graphics change to match your new theme. When you apply styles, your headings change to match the new theme.</p>
